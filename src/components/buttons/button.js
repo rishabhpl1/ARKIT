38e2bd171838fdd7644e0b1eacf8ef619b80051a
Your Name <you@example.com>
2022-01-26 00:26:00 +0530

    Added Button component

---
+++ src/components/buttons/button.js
@@ -0,0 +1,9 @@
+import './button.css'
+const Button = (props) => {
+    return(
+        <div className="button-background">
+        <button className="button button-type-1">Button Type 1</button>
+        </div>
+    )
+}
+export default Button
